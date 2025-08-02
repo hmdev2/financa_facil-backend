b7f7385910ff4a455407308413c96a0f1910df87
hmdev2 <hmdev2@proton.me>
2025-08-02 10:47:55 +0200

    register and login system created

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransactionsController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::get('/dashboard', [TransactionsController::class, 'show']);
-Route::post('/transaction/store', [TransactionsController::class, 'store']);
-Route::delete('transaction/{id}', [TransactionsController::class, 'destroy']);
-Route::put('transaction/{id}', [TransactionsController::class, 'update']);
-Route::get('transaction/{id}', [TransactionsController::class, 'transaction']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+    Route::get('/dashboard', [TransactionsController::class, 'show']);
+    Route::post('/transaction/store', [TransactionsController::class, 'store']);
+    Route::delete('transaction/{id}', [TransactionsController::class, 'destroy']);
+    Route::put('transaction/{id}', [TransactionsController::class, 'update']);
+    Route::get('transaction/{id}', [TransactionsController::class, 'transaction']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
