2d659e68ad608767e0f8357dc0822fd7dd477552
Pedro Melo <hmdev2@proton.me>
2025-08-25 10:59:58 -0300

    validate-token route created

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
-    })->middleware('auth:sanctum');
+    });
     Route::get('/dashboard', [TransactionsController::class, 'show']);
     Route::post('/transaction/store', [TransactionsController::class, 'store']);
     Route::delete('transaction/{id}', [TransactionsController::class, 'destroy']);
     Route::put('transaction/{id}', [TransactionsController::class, 'update']);
     Route::get('transaction/{id}', [TransactionsController::class, 'transaction']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/validate-token', function (Request $request) {
+        return response()->json(['valid' => true]);
+    });
 });
